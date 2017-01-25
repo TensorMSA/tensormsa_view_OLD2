c8df6bef45ce7fc0cc6669b65e7524739752bcf2
ashcroft147 <pd050786@VDIPS.POSCO.DOM>
2017-01-25 19:11:54 +0900

    Add files for publishing

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,9 @@
+import * as d3 from 'd3';
 import '../scss/style.scss';
 
 console.log("Success");
+
+d3.select("li#nm").on("click", function () {
+    let position = 800;
+    console.log(position);
+});
